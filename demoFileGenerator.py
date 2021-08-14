d3dee0d6e070cfe818b1e99f37ab92e5929db368
Ofir Tene <63402158+ofirte@users.noreply.github.com>
2021-08-14 19:24:35 +0300

    Update demoFileGenerator.py

--- demoFileGenerator.py
+++ demoFileGenerator.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     for i in range(1, 200):
         for j in range(1,100000):
             if i == 193 and j == 100:
-                f.write("line{} niddlelfwjfidlsajfkewla;fjekla;f sjdkfudddeawfdjkfjweilsfijlsdkfjeiwlfkdjsielsfkjeislfjesifljiels!".format(i))
+                f.write("line{} needlelfwjfidlsajfkewla;fjekla;f sjdkfudddeawfdjkfjweilsfijlsdkfjeiwlfkdjsielsfkjeislfjesifljiels!".format(i))
             else:
                 f.write("line{} asdfjklfwjfidlsajfkewla;fjekla;f sjdkfudddeawfdjkfjweilsfijlsdkfjeiwlfkdjsielsfkjeislfjesifljiels!".format(i))
         f.write("\n")
